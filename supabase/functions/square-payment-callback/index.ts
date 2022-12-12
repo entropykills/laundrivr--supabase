fae6a84963c0a2256e5a04461ec579d8c2e66ab1
yakovliam <jacob@yakovliam.com>
2022-12-11 22:34:28 -0500

    feat: fix warning

--- supabase/functions/square-payment-callback/index.ts
+++ supabase/functions/square-payment-callback/index.ts
@@ -1,18 +1,7 @@
 import "https://deno.land/x/xhr@0.2.1/mod.ts";
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@1.35.5";
-import { Client, Environment } from "https://esm.sh/square@24.0.0";
 import { stringify } from "../_shared/index.ts";
-
-const squareEnvironment = Deno.env.get("SQUARE_ENVIRONMENT")!;
-
-// const square = new Client({
-//   accessToken: Deno.env.get("SQUARE_ACCESS_TOKEN")!,
-//   environment:
-//     squareEnvironment === "production"
-//       ? Environment.Production
-//       : Environment.Sandbox,
-// });
 
 const supabase = createClient(
   Deno.env.get("SUPABASE_URL")!,
